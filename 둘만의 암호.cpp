1d7046729f54315cfad454ba7b570c1ec73e9553
roonatic <sudo.rwx777@gmail.com>
2023-04-21 23:17:16 +0900

    둘만의 암호

---
+++ 둘만의 암호.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+	string answer = "";
+
+	int n = 26;
+	char start = 'a';
+	vector<char> list;
+	vector<char> no;
+
+	for (int i = 0; i < skip.size(); i++)
+		no.push_back(skip[i]);
+
+	while (n--)
+	{
+		int cnt = 0;
+		for (int i = 0; i < no.size(); i++)
+		{
+			if (no[i] == start)
+			{
+				cnt = 1;
+				break;
+			}
+		}
+
+		if (cnt == 0)
+			list.push_back(start);
+
+		start++;
+	}
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		int idx = 0;
+		for (int j = 0; j < list.size(); j++)
+		{
+			if (list[j] == s[i])
+			{
+				idx = j;
+				break;
+			}
+
+		}
+
+		if (idx + index > list.size() - 1)
+			answer += list[(idx + index) % list.size()];
+		else
+			answer += list[idx + index];
+	}
+	return answer;
+}
+
+int main()
+{
+	string s = "z";
+	string skip = "abcdefghij";
+	int index = 20;
+
+	solution(s, skip, index);
+}
